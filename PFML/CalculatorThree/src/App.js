785c48ef84e9a23e71a9d8b0d64da988f9ed110e
avrilpearl <sienna.avril.pearl@gmail.com>
2019-01-25 14:58:38 -0500

    wip

--- PFML/CalculatorThree/src/App.js
+++ PFML/CalculatorThree/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Header, Footer } from '@massds/mayflower-react';
+import { Header, Footer, InputRadioButton } from '@massds/mayflower-react';
 import UtilityNavData from './data/UtilityNav.data';
 import MainNavData from './data/MainNav.data';
 import HeaderSearchData from './data/HeaderSearch.data';
 import FooterLinksLiveData from './data/FooterLinksLive.json';
 import SocialLinksLiveData from './data/SocialLinksLive.json';
-import QuestionOne from './components/QuestionOne'
+import QuestionOne from './components/QuestionOne';
+import QuestionTwo from './components/QuestionTwo';
+import Output from './components/Output';
 
 import './index.css';
 
@@ -13,6 +15,10 @@ import './index.css';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      yearIncome: null,
+      maxWeeks: null
+    };
     this.footerProps = {
       footerLinks: FooterLinksLiveData.footerLinks,
       socialLinks: SocialLinksLiveData.socialLinks
@@ -26,16 +32,35 @@ class App extends Component {
       siteLogoDomain: { url: { domain: 'https://www.mass.gov/' } }
     };
   }
+
+  handleInput = (numberValue, e) => {
+    this.setState({
+      yearIncome: numberValue
+    });
+  };
+
+  handleRadio = ({selected, maxWeeks, event}) => {
+    this.setState({
+      maxWeeks
+    });
+  }
+
   render() {
+    const questTwoDisabled = this.state.weeks > 0 ? false : true;
     return (
       <div className="App">
         <Header {...this.headerProps} />
           <main className="main-content">
             <section className="main-content--two">
               <div className="ma__page-header__content">
-                <h1 className="ma__page-header__title">Mayflower Starter Template</h1>
+                <h1 className="ma__page-header__title">Paid Family Medical Leave Benefits Caculator</h1>
               </div>
-              <QuestionOne />
+              <QuestionOne error={false} disabled={false} defaultSelected="active-duty" onChange={this.handleRadio} />
+              <QuestionTwo onChange={this.handleInput} disabled={questTwoDisabled} />
+              <hr />
+              {this.state.yearIncome > 0 && this.state.maxWeeks > 0 && (
+                <Output yearIncome={this.state.yearIncome} maxWeeks={this.state.maxWeeks}/>
+              )}  
             </section>
           </main>
         <Footer {...this.footerProps} />
--- PFML/CalculatorThree/src/components/QuestionOne/index.js
+++ PFML/CalculatorThree/src/components/QuestionOne/index.js
@@ -1,49 +1,57 @@
 import React, { Component } from 'react';
-import { InputRadioGoup } from '@massds/mayflower-react';
-//import './index.css';
-
+import { InputRadioGroup, CalloutAlert, Paragraph } from '@massds/mayflower-react';
+import QuestionOneProps from '../../data/QuestionOne.json'
+import './index.css';
 
 class QuestionOne extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      message: "",
+      messageTheme: "",
+      weeks: ""
+    };
+  }
+
+  handleChange = ({selected, name, event}) => {
+    let maxWeeks;
+    QuestionOneProps.options.forEach(option => {
+      if(option.value === selected) {
+        maxWeeks = option.weeks;
+        this.setState({ 
+          message: option.message,
+          messageTheme: option.theme,
+          weeks: option.weeks 
+        });
+      }
+    })
+    if(typeof this.props.onChange == "function"){
+      this.props.onChange({selected, maxWeeks, event})
+    }
   }
 
   render() {
+    const { question, options } = QuestionOneProps;
   	const radioGroupProps = {
-  		title: "Are you leaving for one of the following reasons?",
-  		name: "question-one-reason-for-leave",
+  		title: question,
+  		name: "question-one",
   		outline: true,
   		inline: true,
   		error: this.props.error,
   		disabled: this.props.disabled,
-  		defaultSelected: this.props.defaultSelected,
-  		onChange: ({ selected, name, event }) => {
-  			console.log(selected)
-  		}, 
-  		radioButtons: [{ 
-			text: "Birth or adoption of a child",
-			value: "birth-adoption",
-			message: "You can be covered up to 12 weeks by Paid Family and Medical Leave Program."
-		}, {
-			text: "Own Medical Condition",
-			value: "own-medical",
-			message: "You can be covered up to 20 weeks by Paid Family and Medical Leave Program."
-		}, {
-			text: "Caring for a Loved One",
-			value: "caring-loved-one",
-			message: "You can be covered up to 20 weeks by Paid Family and Medical Leave Program."
-		}, {
-			text: "Active Duty",
-			value: "active-duty",
-			message: "You can be covered up to 26 weeks by Paid Family and Medical Leave Program."
-		}, {
-			text: "Other",
-			value: "other",
-			message: "You may not be covered by the Paid Family and Medical Leave Program, for more information go here."
-		}]
+  		//defaultSelected: this.props.defaultSelected,
+  		onChange: this.handleChange, 
+  		radioButtons: options
   	}
     return (
-      <InputRadioGoup {...radioGroupProps} />
+      <React.Fragment>
+        <InputRadioGroup {...radioGroupProps} />
+        {this.state.message && (
+          <CalloutAlert theme={this.state.messageTheme} icon={{name: this.state.messageTheme === "c-error-red" ? "alert" : "",ariaHidden: true}} >
+            <Paragraph text={this.state.message} />
+          </CalloutAlert>
+        )}
+      </React.Fragment>
     );
   }
 }
