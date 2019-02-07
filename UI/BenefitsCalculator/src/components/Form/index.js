25c7202f580d71bfe896f0a623a78897fc578f51
Minghua Sun <clairesunstudio@gmail.com>
2019-02-07 13:57:44 -0500

    calculate weekly benefits

--- UI/BenefitsCalculator/src/components/Form/index.js
+++ UI/BenefitsCalculator/src/components/Form/index.js
@@ -14,7 +14,8 @@ class Form extends Component {
         quarter2: '',
         quarter3: '',
         quarter4: ''
-      }
+      },
+      submitted: false
     };
   }
 
@@ -38,60 +39,65 @@ class Form extends Component {
     };
     return(
       <Fragment>
-      <form className="ma__form-page" action="#">
-        <InputCurrency
-          {... inputCurrencyProps}
-          labelText="October 2018 – December 2018 earnings:"
-          id="quarter1"
-          name="quarter1"
-          defaultValue={quarter1}
-          onChange={(e, value) => {
-            const newStateValue = { ...stateValue };
-            newStateValue.quarter1 = value;
-            this.setState({ value: newStateValue });
-          }}
-        />
-        <InputCurrency
-          {... inputCurrencyProps}
-          labelText="October 2018 – December 2018 earnings:"
-          id="quarter2"
-          name="quarter2"
-          defaultValue={quarter2}
-          onChange={(e, value) => {
-            const newStateValue = { ...stateValue };
-            newStateValue.quarter2 = value;
-            this.setState({ value: newStateValue });
-          }}
-        />
-        <InputCurrency
-          {... inputCurrencyProps}
-          labelText="October 2018 – December 2018 earnings:"
-          id="quarter3"
-          name="quarter3"
-          defaultValue={quarter3}
-          onChange={(e, value) => {
-            const newStateValue = { ...stateValue };
-            newStateValue.quarter3 = value;
-            this.setState({ value: newStateValue });
-          }}
-        />
-        <InputCurrency
-          {... inputCurrencyProps}
-          labelText="October 2018 – December 2018 earnings:"
-          id="quarter4"
-          name="quarter4"
-          defaultValue={quarter4}
-          onChange={(e, value) => {
-            const newStateValue = { ...stateValue };
-            newStateValue.quarter4 = value;
-            this.setState({ value: newStateValue });
-          }}
-        />
-        <Button
-          text="See Benefits"
-        />
-      </form>
-      <Output { ...stateValue }/>
+        <form className="ma__form-page" action="#">
+          <InputCurrency
+            {... inputCurrencyProps}
+            labelText="October 2018 – December 2018 earnings:"
+            id="quarter1"
+            name="quarter1"
+            defaultValue={quarter1}
+            onChange={(e, value) => {
+              const newStateValue = { ...stateValue };
+              newStateValue.quarter1 = value;
+              this.setState({ value: newStateValue });
+            }}
+          />
+          <InputCurrency
+            {... inputCurrencyProps}
+            labelText="October 2018 – December 2018 earnings:"
+            id="quarter2"
+            name="quarter2"
+            defaultValue={quarter2}
+            onChange={(e, value) => {
+              const newStateValue = { ...stateValue };
+              newStateValue.quarter2 = value;
+              this.setState({ value: newStateValue });
+            }}
+          />
+          <InputCurrency
+            {... inputCurrencyProps}
+            labelText="October 2018 – December 2018 earnings:"
+            id="quarter3"
+            name="quarter3"
+            defaultValue={quarter3}
+            onChange={(e, value) => {
+              const newStateValue = { ...stateValue };
+              newStateValue.quarter3 = value;
+              this.setState({ value: newStateValue });
+            }}
+          />
+          <InputCurrency
+            {... inputCurrencyProps}
+            labelText="October 2018 – December 2018 earnings:"
+            id="quarter4"
+            name="quarter4"
+            defaultValue={quarter4}
+            onChange={(e, value) => {
+              const newStateValue = { ...stateValue };
+              newStateValue.quarter4 = value;
+              this.setState({ value: newStateValue });
+            }}
+          />
+          <Button
+            text="See Benefits"
+            onClick={() => this.setState({ submitted: true })}
+          />
+        </form>
+        {
+          this.state.submitted && (
+            <Output {...stateValue} />
+          )
+        }
       </Fragment>
     );
   }
--- UI/BenefitsCalculator/src/components/Form/output.js
+++ UI/BenefitsCalculator/src/components/Form/output.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { CalloutAlert, HelpTip } from '@massds/mayflower-react';
 import { toCurrency } from '../../util';
 
 const Output = (props) => {
-  const { quarter1, quarter2, quarter3, quarter4 } = props;
+  const {
+    quarter1, quarter2, quarter3, quarter4
+  } = props;
   const quartersHaveValue = [quarter1, quarter2, quarter3, quarter4].filter((q) => typeof q === 'number' && q > 0);
+  const quartersCount = quartersHaveValue.length;
   let topQuarters;
-  if (quartersHaveValue.length > 2) {
-    topQuarters = quartersHaveValue.sort((q1, q2) => q1 - q2).slice(0,2);
-  } else if (quartersHaveValue.length > 0) {
-    topQuarters = quartersHaveValue.sort((q1, q2) => q1 - q2).slice(0,1);
+  let weeksInTopQuarters = 26;
+  if (quartersCount > 2) {
+    topQuarters = quartersHaveValue.sort((q1, q2) => q2 - q1).slice(0, 2);
+  } else if (quartersCount > 0) {
+    topQuarters = quartersHaveValue.sort((q1, q2) => q2 - q1).slice(0, 1);
+    weeksInTopQuarters = 13;
   }
-  //console.log(quartersHaveValue)
-  console.log(topQuarters)
+  const sum = (a, b) => a + b;
+  const topQuartersSum = topQuarters && topQuarters.length > 0 && topQuarters.reduce(sum);
+  const weeklyBenefit = 1 / 2 * topQuartersSum / weeksInTopQuarters;
+  const weeklyBenefitMax = 795;
+  const weeklyBenefitFinal = weeklyBenefit > weeklyBenefitMax ? weeklyBenefitMax : weeklyBenefit;
   return(
     <CalloutAlert theme="c-primary" icon={null}>
       <HelpTip
         theme="c-white"
         textBefore="You would be eligible to receive "
-        triggerText={`<span>${toCurrency(30)} for 30 weeks</span>`}
+        triggerText={`<span>${toCurrency(weeklyBenefitFinal)} for 30 weeks</span>`}
         textAfter=", based on your maximum benefit credit."
         id="help-tip-benefits"
         labelID="help-tip-benefits-label"
-      >
-      </HelpTip>
+      />
     </CalloutAlert>
-  )
-}
+  );
+};
 
 export default Output;
