9c438dda8b4828a872da90238fbc46c200375733
Minghua Sun <clairesunstudio@gmail.com>
2019-02-07 14:13:35 -0500

    calculate max and duration

--- UI/BenefitsCalculator/src/components/Form/output.js
+++ UI/BenefitsCalculator/src/components/Form/output.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { CalloutAlert, HelpTip } from '@massds/mayflower-react';
 import { toCurrency } from '../../util';
 
+const sum = (a, b) => a + b;
+
 const Output = (props) => {
   const {
     quarter1, quarter2, quarter3, quarter4
   } = props;
+
+  // weekly benefit
   const quartersHaveValue = [quarter1, quarter2, quarter3, quarter4].filter((q) => typeof q === 'number' && q > 0);
   const quartersCount = quartersHaveValue.length;
   let topQuarters;
@@ -16,18 +20,26 @@ const Output = (props) => {
     topQuarters = quartersHaveValue.sort((q1, q2) => q2 - q1).slice(0, 1);
     weeksInTopQuarters = 13;
   }
-  const sum = (a, b) => a + b;
   const topQuartersSum = topQuarters && topQuarters.length > 0 && topQuarters.reduce(sum);
   const weeklyBenefit = 1 / 2 * topQuartersSum / weeksInTopQuarters;
   const weeklyBenefitMax = 795;
   const weeklyBenefitFinal = weeklyBenefit > weeklyBenefitMax ? weeklyBenefitMax : weeklyBenefit;
+
+  // max benefit credit
+  const maxBenefitOption1 = 30 * weeklyBenefitFinal;
+  const maxBenefitOption2 = quartersHaveValue.reduce(sum) * 0.36;
+  const maxBenefitFinal = maxBenefitOption1 > maxBenefitOption2 ? maxBenefitOption2 : maxBenefitOption1;
+
+  // benefit duration
+  const benefitDuration = maxBenefitFinal / weeklyBenefitFinal;
+
   return(
     <CalloutAlert theme="c-primary" icon={null}>
       <HelpTip
         theme="c-white"
         textBefore="You would be eligible to receive "
-        triggerText={`<span>${toCurrency(weeklyBenefitFinal)} for 30 weeks</span>`}
-        textAfter=", based on your maximum benefit credit."
+        triggerText={`<span>${toCurrency(weeklyBenefitFinal)} for ${parseInt(benefitDuration, 10)} weeks</span>`}
+        textAfter={`, based on your maximum benefit credit of ${toCurrency(maxBenefitFinal)}.`}
         id="help-tip-benefits"
         labelID="help-tip-benefits-label"
       />
