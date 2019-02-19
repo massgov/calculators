32788cf17351ec22d38aaff2a5f1918d42f2f206
Minghua Sun <clairesunstudio@gmail.com>
2019-02-19 16:33:50 -0500

    clean up table data template

--- PFML/ContributionCalculator/src/data/table.data.js
+++ PFML/ContributionCalculator/src/data/table.data.js
@@ -22,42 +22,24 @@ export default {
         heading: true,
         text: 'The employer will pay:'
       }, {
-        heading: false,
-        text: '$27.50'
       }, {
-        heading: false,
-        text: '$27.50'
       }, {
-        heading: false,
-        text: '$27.50'
       }]
     }, {
       cells: [{
         heading: true,
         text: 'The employee will pay:'
       }, {
-        heading: false,
-        text: '$27.50'
       }, {
-        heading: false,
-        text: '$27.50'
       }, {
-        heading: false,
-        text: '$27.50'
       }]
     }, {
       cells: [{
         heading: true,
         text: 'Total Contribution'
       }, {
-        heading: false,
-        text: '$27.50'
       }, {
-        heading: false,
-        text: '$27.50'
       }, {
-        heading: false,
-        text: '$27.50'
       }]
     }]
   }]
