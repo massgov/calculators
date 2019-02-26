a8d060aa84c83f507727c7145e6804c0c47c14e6
Steven Murray <smurrayatwork@gmail.com>
2019-02-26 15:54:10 -0500

    Update UI/PartTimeBenefitsCalculator/src/components/Calculator/Output2.js
    Co-Authored-By: clairesunstudio <clairesunstudio@gmail.com>

--- UI/PartTimeBenefitsCalculator/src/components/Calculator/Output2.js
+++ UI/PartTimeBenefitsCalculator/src/components/Calculator/Output2.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import {
   CalloutAlert, HelpTip, Paragraph, Input, InputContext
 } from '@massds/mayflower-react';
