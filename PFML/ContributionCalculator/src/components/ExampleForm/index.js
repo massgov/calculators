132d68695fbaeaf39fc70ac4765b4dd8e010c344
Minghua Sun <clairesunstudio@gmail.com>
2019-02-26 16:31:10 -0500

    lint-fix

--- PFML/ContributionCalculator/src/components/ExampleForm/index.js
+++ PFML/ContributionCalculator/src/components/ExampleForm/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { decode, addUrlProps, UrlQueryParamTypes } from 'react-url-query';
 import { FormContext } from '@massds/mayflower-react';
