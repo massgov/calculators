00eb2780eaec9e9667fd9ec6afb0b0328482d3d0
Steven Murray <smurrayatwork@gmail.com>
2019-02-26 15:54:51 -0500

    Update UI/PartTimeBenefitsCalculator/src/components/Calculator/Output2.js
    Co-Authored-By: clairesunstudio <clairesunstudio@gmail.com>

--- UI/PartTimeBenefitsCalculator/src/components/Calculator/Output2.js
+++ UI/PartTimeBenefitsCalculator/src/components/Calculator/Output2.js
@@ -115,7 +115,7 @@ export const ScenarioTwo = (props) => {
 export const ScenarioThree = (props) => {
   // Do not make a copy of formContext with object destructuring.
   // eslint-disable-next-line react/destructuring-assignment
-  const formContext = props.formContext;
+  const formContext = useContext(FormContext);
   // These are default values only.
   const scenarioDefaults = {
     showScenario: false,
