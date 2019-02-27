e1c9bca941b9c7f40ed75abb6afaa81578f6c46c
avrilpearl <sienna.avril.pearl@gmail.com>
2019-02-27 14:31:04 -0500

    revet env variable change

--- PFML/ContributionCalculator/src/components/Form/Reset.js
+++ PFML/ContributionCalculator/src/components/Form/Reset.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Reset = () => (
-  <div className="ma__reset">
-    <hr />
-    <a href={process.env.PUBLIC_URL ? `${process.env.PUBLIC_URL}` : '/'}>Start Over</a>
-  </div>
-);
+const Reset = () => {
+  return(
+    <div className="ma__reset">
+      <hr />
+      <a href={process.env.PUBLIC_URL}>Start Over</a>
+    </div>
+  )
+}
+  
 
 export default Reset;
