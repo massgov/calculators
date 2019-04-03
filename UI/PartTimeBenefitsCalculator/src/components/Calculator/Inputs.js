3fb364a7fb8a08e5a4b4882cca78ad56659e00bc
Minghua Sun <clairesunstudio@gmail.com>
2019-04-03 10:05:10 -0400

    fix weekly benefits input

--- UI/PartTimeBenefitsCalculator/src/components/Calculator/Inputs.js
+++ UI/PartTimeBenefitsCalculator/src/components/Calculator/Inputs.js
@@ -6,7 +6,7 @@ export const QuestionOne = (props) => {
   const { handleChange } = props;
   return(
     <InputCurrency
-      labelText="How much do you receive weekly in UI benefits?"
+      labelText="What is your weekly benefit amount?"
       inline
       required
       id="weekly-benefits"
@@ -17,6 +17,7 @@ export const QuestionOne = (props) => {
       min={0}
       max={795}
       onChange={handleChange}
+      onBlur={(val) => handleChange(val, 'weekly-benefits')}
     />
   );
 };
