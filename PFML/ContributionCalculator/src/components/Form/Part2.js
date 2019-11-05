655ede643b6ae80b9afb5897472cd945b946e10b
avrilpearl <sienna.avril.pearl@gmail.com>
2019-11-05 11:14:41 -0500

    update rendered numbers

--- PFML/ContributionCalculator/src/components/Form/Part2.js
+++ PFML/ContributionCalculator/src/components/Form/Part2.js
@@ -167,7 +167,7 @@ const Part2 = (props) => {
                               [`${toCurrency(famCompPayment + medCompPayment)} = ${toCurrency(totalPayroll)} X ${toPercentage(totalPercent, 2)}`]
                             ) : (
                               // under 25 total medLeave calculation
-                              [`${toCurrency(famCompPayment + medCompPayment)} = (${toCurrency(totalPayroll)} X ${toPercentage(famPercent, 2)}) + (${toCurrency(totalPayroll)} X ${toPercentage(medPercent, 2)} X ${empMedContPercent})`]
+                              [`${toCurrency(famCompPayment + medCompPayment)} = (${toCurrency(totalPayroll)} X ${toPercentage(famPercent, 5)}) + (${toCurrency(totalPayroll)} X ${toPercentage(medPercent, 5)} X ${empMedContPercent})`]
                             )
                             }
                           />
@@ -179,10 +179,10 @@ const Part2 = (props) => {
                               theme="c-white"
                             >
                               <div className="ma__help-text">
-                                Family Leave: {toCurrency(famPercent * totalPayroll)} = {toCurrency(totalPayroll)} X {toPercentage(famPercent, 2)}
+                                Family Leave: {toCurrency(famPercent * totalPayroll)} = {toCurrency(totalPayroll)} X {toPercentage(famPercent, 5)}
                               </div>
                               <div className="ma__help-text">
-                                Medical Leave: {toCurrency(medPercent * totalPayroll * medPayrollPercent)} = {toCurrency(totalPayroll)} X { over25 ? toPercentage(medPercent, 2) : <span>{toPercentage(medPercent, 2)} X {empMedContPercent}</span>}
+                                Medical Leave: {toCurrency(medPercent * totalPayroll * medPayrollPercent)} = {toCurrency(totalPayroll)} X { over25 ? toPercentage(medPercent, 5) : <span>{toPercentage(medPercent, 5)} X {empMedContPercent}</span>}
                               </div>
                             </HelpTip>
                           </p>
@@ -239,10 +239,10 @@ const Part2 = (props) => {
                               id="help-tip-tot-emp-ann-cont"
                               helpText={over25 ? (
                                 // over 25 total medLeave calculation
-                                [`Total Contribution: ${toCurrency(famPayment + medPayment)} = ${toCurrency(payrollWagesCap)} X ${toPercentage(totalPercent, 2)}`]
+                                [`Total Contribution: ${toCurrency(famPayment + medPayment)} = ${toCurrency(payrollWagesCap)} X ${toPercentage(totalPercent, 5)}`]
                               ) : (
                                 // under 25 total medLeave calculation
-                                [`Total Contribution: ${toCurrency(famPayment + medPayment)} = (${toCurrency(payrollWagesCap)} X ${toPercentage(famPercent, 2)}) + (${toCurrency(payrollWagesCap)} X ${toPercentage(medPercent, 2)} X ${empMedContPercent})`]
+                                [`Total Contribution: ${toCurrency(famPayment + medPayment)} = (${toCurrency(payrollWagesCap)} X ${toPercentage(famPercent, 5)}) + (${toCurrency(payrollWagesCap)} X ${toPercentage(medPercent, 5)} X ${empMedContPercent})`]
                               )
                               }
                               theme="c-white"
@@ -253,9 +253,9 @@ const Part2 = (props) => {
                               id="help-tip-medfam-emp-ann-cont"
                               theme="c-white"
                             >
-                              <div className="ma__help-text">Family Leave: {toCurrency(famPayment)} = {toCurrency(payrollWagesCap)} X {toPercentage(famPercent, 2)}
+                              <div className="ma__help-text">Family Leave: {toCurrency(famPayment)} = {toCurrency(payrollWagesCap)} X {toPercentage(famPercent, 5)}
                               </div>
-                              <div className="ma__help-text">Medical Leave: {toCurrency(medPayment)} = {toCurrency(payrollWagesCap)} X { over25 ? toPercentage(medPercent, 2) : <span>{toPercentage(medPercent, 2)} X {empMedContPercent}</span>}
+                              <div className="ma__help-text">Medical Leave: {toCurrency(medPayment)} = {toCurrency(payrollWagesCap)} X { over25 ? toPercentage(medPercent, 5) : <span>{toPercentage(medPercent, 5)} X {empMedContPercent}</span>}
                               </div>
                             </HelpTip>
                           </p>
