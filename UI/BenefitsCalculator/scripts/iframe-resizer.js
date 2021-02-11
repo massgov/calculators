3d0c7bc390a7ddfc03dcb613de0e6571d1bdb2b1
Minghua Sun <clairesunstudio@gmail.com>
2021-02-11 18:13:18 -0500

    wip

--- UI/BenefitsCalculator/scripts/iframe-resizer.js
+++ UI/BenefitsCalculator/scripts/iframe-resizer.js
@@ -5,11 +5,16 @@ const cheerio = require('cheerio');
 const indexPath = path.resolve(__dirname, '../public/index.html');
 
 const indexHTML = fs.readFileSync(indexPath);
-const script = '<script type="text/javascript" src="https://massgovfeature3.prod.acquia-sites.com/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js"></script> />'
-const $ = cheerio.load(indexHTML);
+// fs.readFile(indexPath, function(err, data) {
+//     const c = new Buffer.from(data).toString()
+//     console.log(c)})
+const c = new Buffer.from(indexHTML).toString()
+const script = '<script type="text/javascript" src="https://massgovfeature3.prod.acquia-sites.com/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js"></script>'
+const $ = cheerio.load(c);
 
-const content = $('body').append(script)
+$('body').append(script);
+console.log($.html())
 
-fs.writeFile(indexHTML, content.html(), (err) => {
+fs.writeFile(indexPath, $.html(), (err) => {
     if (err) throw err;
 })
