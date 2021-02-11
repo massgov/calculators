e3d4123081def24a2780de7b1321cb70f3a6ff98
Minghua Sun <clairesunstudio@gmail.com>
2021-02-11 18:39:21 -0500

    wip

--- UI/BenefitsCalculator/scripts/iframe-resizer.js
+++ UI/BenefitsCalculator/scripts/iframe-resizer.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const cheerio = require('cheerio');
+const { html_beautify } = require('js-beautify');
 
 const indexPath = path.resolve(__dirname, '../public/index.html');
 
@@ -12,9 +13,15 @@ const c = new Buffer.from(indexHTML).toString()
 const script = '<script type="text/javascript" src="https://massgovfeature3.prod.acquia-sites.com/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js"></script>'
 const $ = cheerio.load(c);
 
-$('body').append(script);
-console.log($.html())
+if ($.html().includes(script)) {
+    return true
+} else {
+    $('body').append(script);
+}
 
-fs.writeFile(indexPath, $.html(), (err) => {
+const finalHTML = html_beautify($.html(), { indent_size:2 })
+console.log(finalHTML)
+
+fs.writeFile(indexPath, finalHTML, (err) => {
     if (err) throw err;
 })
