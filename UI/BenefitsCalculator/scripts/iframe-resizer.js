0ddc0360b7218ea53195a87657df1267a85ea7a1
Minghua Sun <clairesunstudio@gmail.com>
2021-02-12 15:48:25 -0500

    lint fix

--- UI/BenefitsCalculator/scripts/iframe-resizer.js
+++ UI/BenefitsCalculator/scripts/iframe-resizer.js
@@ -1,23 +1,27 @@
 const path = require('path');
 const fs = require('fs');
+// eslint-disable-next-line
 const cheerio = require('cheerio');
+// eslint-disable-next-line
 const { html_beautify } = require('js-beautify');
 
 const indexPath = path.resolve(__dirname, '../public/index.html');
 
 const indexHTML = fs.readFileSync(indexPath);
 
-const script = '<script type="text/javascript" src="https://massgovfeature3.prod.acquia-sites.com/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js"></script>'
+const script = '<script type="text/javascript" src="https://massgovfeature3.prod.acquia-sites.com/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js"></script>';
 const $ = cheerio.load(indexHTML);
 
 if ($.html().includes(script)) {
-    return true
-} else {
-    $('body').append(script);
+  return true;
 }
+$('body').append(script);
 
-const finalHTML = html_beautify($.html(), { indent_size:2 })
+
+const finalHTML = html_beautify($.html(), { indent_size: 2 });
 
 fs.writeFile(indexPath, finalHTML, (err) => {
-    if (err) throw err;
-})
+  if (err) throw err;
+});
+
+return false;
